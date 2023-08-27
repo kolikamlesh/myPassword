c5ecb5885ec4fa00a31565b932fe40135bbe017d
kamlesh <45515891+kolikamlesh@users.noreply.github.com>
2023-08-28 02:42:21 +0530

    Update connection.js
    mysql2 is added

--- startup/connection.js
+++ startup/connection.js
@@ -1,5 +1,5 @@
 // Load module mysql connection
-var mysql = require('mysql');
+var mysql = require('mysql2');
 var { database } = require('../config/default');
 
 var connection = mysql.createConnection(database);    
